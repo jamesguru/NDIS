82f697393bdb8ff3deb4fade4cf88825646e21a3
unknown <jameskagunga2022@gmail.com>
2023-12-07 22:55:39 -0800

    added changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,28 +1,49 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Admin from "./pages/admin/Admin";
 import Staff from "./pages/staff/Staff";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
 
 function App() {
+
+  const Layout = () => {
+    return (
+      <div className="app">
+        
+          <Navbar />
+          <Outlet />
+          <Footer />
+       
+      </div>
+    );
+  };
+
   const router = createBrowserRouter([
     {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/login",
-      element: <Login />,
-    },
-
-    {
-      path: "/admin",
-      element: <Admin />,
-    },
-    {
-      path: "/staff",
-      element: <Staff />,
+      path:"/",
+      element:<Layout />,
+      children:[
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/login",
+          element: <Login />,
+        },
+    
+        {
+          path: "/admin",
+          element: <Admin />,
+        },
+        {
+          path: "/staff",
+          element: <Staff />,
+        }
+      ]
     }
   ]);
 
