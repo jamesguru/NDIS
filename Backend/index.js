9effa39c5fa87e62e62cdd921e4fc4010160155c
unknown <jameskagunga2022@gmail.com>
2023-12-20 17:07:57 -0800

    added backend changes

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const authRoute = require("./routes/auth");
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/api/v1/auth", authRoute);
 
 const PORT=process.env.PORT;
 app.listen(PORT,()=>{
---
+++ Backend/models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const UserSchema = mongoose.Schema({
+    username:{type:String, require:true},
+    fullname:{type:String, require:true},
+    email:{type:String, require:true, unique:true},
+    phone:{type:Number, require:true},
+    address:{type:string, require:true},
+    gender:{type:String, require:true},
+    password:{type:String, required:true},
+    isAdmin:{type:String, default: false},
+    isActive:{type:boolean, default:true},
+    img:{type:String},
+
+},{timestamps:true})
+
+module.exports = mongoose.model("User", UserSchema);
