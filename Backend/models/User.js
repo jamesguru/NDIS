1e0cf4cbf49fe0843934f01961086d868aa74f6e
unknown <jameskagunga2022@gmail.com>
2023-12-20 17:13:16 -0800

    added backend changes

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -4,11 +4,11 @@ const UserSchema = mongoose.Schema({
     fullname:{type:String, require:true},
     email:{type:String, require:true, unique:true},
     phone:{type:Number, require:true},
-    address:{type:string, require:true},
+    address:{type:String, require:true},
     gender:{type:String, require:true},
     password:{type:String, required:true},
     isAdmin:{type:String, default: false},
-    isActive:{type:boolean, default:true},
+    isActive:{type:Boolean, default:true},
     img:{type:String},
 
 },{timestamps:true})
